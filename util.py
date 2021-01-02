2fc131d08f26f0200ab2f85aa9a1f5ce26dcde24
Max Rees <maxcrees@me.com>
2021-01-02 03:38:12 -0500

    fix: wrong dir when getting snapshot version
    (cherry picked from commit 23dc9d246053d6af74b5b79ad3082252491945d1)

--- util.py
+++ util.py
@@ -21,6 +21,8 @@ def solve_filepath(path):
 
     if path[0] == '/':
         return path
+    elif os.path.exists(path):
+        return path
     else:
         mydir = os.path.dirname(os.path.realpath(__file__))
         return mydir + '/' + path
